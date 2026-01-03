7ab0a0f540cfc9f25041dd7bb27b7e4762feb008
TaskForge Dev <taskforge@local>
2026-01-03 07:16:48 +0300

    fix: preserve HttpError details

--- src/utils/httpError.js
+++ src/utils/httpError.js
@@ -1,9 +1,10 @@
 class HttpError extends Error {
-  constructor({ status, code, message }) {
+  constructor({ status, code, message, details }) {
     super(message);
     this.name = 'HttpError';
     this.status = status;
     this.code = code;
+    this.details = details;
 
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, HttpError);
